d9c67d2043e4df56cff69f3255f3981ed12900f7
sblablaha <sblablaha@gmail.com>
2024-10-01 18:33:56 +0200

    feat(rework-ui): remove counters and other video types from home
    Signed-off-by: sblablaha <sblablaha@gmail.com>

--- internal/controller/home.go
+++ internal/controller/home.go
@@ -9,40 +9,13 @@ import (
 )
 
 func (c *Controller) Home(g *gin.Context) {
-	// get clips
-	var clips []model.Video
-	c.datastore.Where("type = ?", "c").Limit(12).Offset(0).Order("created_at desc").Find(&clips)
-
 	// get videos
 	var videos []model.Video
-	c.datastore.Where("type = ?", "v").Limit(12).Offset(0).Order("created_at desc").Find(&videos)
-
-	// get movies
-	var movies []model.Video
-	c.datastore.Where("type = ?", "m").Limit(12).Offset(0).Order("created_at desc").Find(&movies)
-
-	//TODO: get comics
-
-	// get counts
-	var (
-		clipCount  int64
-		movieCount int64
-		videoCount int64
-	)
-	c.datastore.Table("videos").Where("type = ?", "c").Count(&clipCount)
-	c.datastore.Table("videos").Where("type = ?", "m").Count(&movieCount)
-	c.datastore.Table("videos").Where("type = ?", "v").Count(&videoCount)
+	c.datastore.Where("type = ?", "v").Order("created_at desc").Find(&videos)
 
 	g.HTML(http.StatusOK, "home/home.html", gin.H{
-		"User":   g.MustGet("user").(*model.User),
-		"Clips":  clips,
-		"Movies": movies,
-		"Videos": videos,
-		"Counts": map[string]int64{
-			"clips":  clipCount,
-			"videos": videoCount,
-			"movies": movieCount,
-			"comics": 0,
-		},
+		"User":      g.MustGet("user").(*model.User),
+		"Videos":    videos,
+		"VideoType": "video",
 	})
 }
