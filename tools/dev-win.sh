8f9440249b363e173ce0edaf49c89a14400da5f1
sblablaha <sblablaha@gmail.com>
2025-04-17 07:59:20 +0200

    fix(tools): stop air launching latest build on error
    Signed-off-by: sblablaha <sblablaha@gmail.com>

--- tools/dev-win.sh
+++ tools/dev-win.sh
@@ -3,4 +3,4 @@
 export ZT_DB_DRIVER="sqlite"
 export ZT_DB_CONNSTRING="zt.db"
 export ZT_MEDIA_PATH="test_data"
-exec air --build.cmd 'go build -o tmp/zt.exe main.go' --build.bin 'tmp\zt.exe' --build.log 'tmp\logs.txt'
+exec air --build.cmd 'go build -o tmp/zt.exe main.go' --build.bin 'tmp\zt.exe' --build.log 'tmp\logs.txt' -build.stop_on_error true
