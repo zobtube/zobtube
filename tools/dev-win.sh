8b30202c8c9427863809c6a48491e391fc5c7e13
sblablaha <sblablaha@gmail.com>
2025-08-11 22:56:12 +0200

    feat: add live reload
    Signed-off-by: sblablaha <sblablaha@gmail.com>

--- tools/dev-win.sh
+++ tools/dev-win.sh
@@ -3,4 +3,4 @@
 export ZT_DB_DRIVER="sqlite"
 export ZT_DB_CONNSTRING="zt.db"
 export ZT_MEDIA_PATH="test_data"
-exec air --build.cmd 'go build -o tmp/zt.exe main.go' --build.bin 'tmp\zt.exe' --build.log 'tmp\logs.txt' -build.stop_on_error true -build.include_ext go,html,js,css
+exec air --build.cmd 'go build -o tmp/zt.exe main.go' --build.bin 'tmp\zt.exe' --build.log 'tmp\logs.txt' -build.stop_on_error true -build.include_ext go,html,js,css -proxy.enabled true -proxy.proxy_port 8069 -proxy.app_port 8080
