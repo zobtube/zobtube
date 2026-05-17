fa7cd29c8209d2cc4dd21cf107951d1fe6e446ec
sblablaha <sblablaha@gmail.com>
2026-05-17 23:23:13 +0200

    fix(category/view): only display videos once
    Signed-off-by: sblablaha <sblablaha@gmail.com>

--- web/static/js/components/zt-category-view.js
+++ web/static/js/components/zt-category-view.js
@@ -16,9 +16,20 @@ ZtCategoryView.prototype.connectedCallback = function() {
       var name = (sub.Name||sub.name||"").replace(/&/g,"&amp;").replace(/</g,"&lt;");
       var videos = sub.Videos || sub.videos || [];
       var actors = sub.Actors || sub.actors || [];
+      var byId = {};
+      videos.forEach(function(v) {
+        var vid = v.ID || v.id;
+        if (vid) byId[vid] = v;
+      });
+      actors.forEach(function(act) {
+        (act.Videos || act.videos || []).forEach(function(v) {
+          var vid = v.ID || v.id;
+          if (vid && !byId[vid]) byId[vid] = v;
+        });
+      });
+      var uniqueVideos = Object.keys(byId).map(function(k) { return byId[k]; });
       var html = '<h2 class="card-title actor_name">Category: '+name+'</h2><hr /><div class="row row-cols-1 row-cols-md-6 g-4">';
-      videos.forEach(function(v){ html += (window.ztThumbPreviewHtml||function(){return"";})(v); });
-      actors.forEach(function(act){ (act.Videos||act.videos||[]).forEach(function(v){ html += (window.ztThumbPreviewHtml||function(){return"";})(v); }); });
+      uniqueVideos.forEach(function(v) { html += (window.ztThumbPreviewHtml || function() { return ""; })(v); });
       html += "</div>";
       self.innerHTML = html;
       if (window.zt && window.zt.pageReady) window.zt.pageReady(self);
